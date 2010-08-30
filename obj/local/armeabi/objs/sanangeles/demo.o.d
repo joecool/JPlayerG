5d7243d17f964e31865554e59a88c664b739cf20
Joe Wong <lw.wang@gmail.com>
2010-08-30 15:44:30 +0800

    .

---
+++ obj/local/armeabi/objs/sanangeles/demo.o.d
@@ -0,0 +1,104 @@
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/obj/local/armeabi/objs/sanangeles/demo.o: \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/demo.c \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdlib.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/string.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/malloc.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/alloca.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/strings.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/types.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdint.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/types.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/types.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/memory.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/math.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/limits.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/assert.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/importgl.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/GLES/gl.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/GLES/glplatform.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/KHR/khrplatform.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/app.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/shapes.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/cams.h
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdlib.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/string.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/malloc.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/alloca.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/strings.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdint.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/memory.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/math.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/limits.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/assert.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/importgl.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/GLES/gl.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/GLES/glplatform.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/KHR/khrplatform.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/app.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/shapes.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/cams.h:
---
+++ obj/local/armeabi/objs/sanangeles/importgl.o.d
@@ -0,0 +1,23 @@
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/obj/local/armeabi/objs/sanangeles/importgl.o: \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/importgl.c \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/importgl.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/GLES/gl.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/GLES/glplatform.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/KHR/khrplatform.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdint.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/_types.h
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/samples/san-angeles/jni/importgl.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/GLES/gl.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/GLES/glplatform.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/KHR/khrplatform.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/stdint.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/d/Tools/Android/NDK/android-ndk-r4b/build/platforms/android-4/arch-arm/usr/include/machine/_types.h:
